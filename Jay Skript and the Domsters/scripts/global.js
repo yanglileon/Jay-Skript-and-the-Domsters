62a586f99395c1b1f07c16b939f892c62d9088be
PC201607111005\Administrator <yang.li.lc.leon@gmail.com>
2017-08-01 15:48:31 +0800

    1.添加导航栏选中状态对应的css(here类) 2.添加highlightPage函数,函数作用:判断导航栏中的链接是否被当前网页url包含在内

--- Jay Skript and the Domsters/scripts/global.js
+++ Jay Skript and the Domsters/scripts/global.js
@@ -1,6 +1,6 @@
 /**
  * 共享onload函数
- * @param {*} func 需要共享的函数
+ * @param {*} func 
  */
 function addLoadEvent(func) {
     var oldonload = window.onload;
@@ -16,8 +16,8 @@ function addLoadEvent(func) {
 
 /**
  * 将新节点添加到指定节点的后面
- * @param {*} newElement 新节点
- * @param {*} targetElement 指定节点
+ * @param {*} newElement 
+ * @param {*} targetElement 
  */
 function insertAfter(newElement, targetElement) {
     var parent = targetElement.parentNode;
@@ -28,6 +28,11 @@ function insertAfter(newElement, targetElement) {
     }
 }
 
+/**
+ * 添加class
+ * @param {*} element 
+ * @param {*} value 
+ */
 function addClass(element, value) {
     if (!element.className) {
         element.className = value;
@@ -38,3 +43,32 @@ function addClass(element, value) {
         element.className = newClassName;
     }
 }
+
+/**
+ * 设置导航选中状态
+ */
+function hightlightPage() {
+    if (!document.getElementsByTagName) return false;
+    if (!document.getElementById) return false;
+    // 获取header标签组
+    var headers = document.getElementsByTagName("header");
+    if (headers.length == 0) return false;
+    // 获取nav标签组
+    var navs = headers[0].getElementsByTagName("nav");
+    if (navs.length == 0) return false;
+    // 获取链接数组
+    var links = navs[0].getElementsByTagName("a");
+    if (links.length == 0) return false;
+    var linkurl;
+    for (var i = 0; i < links.length; i++) {
+        linkurl = links[i].getAttribute("href");
+        // 获取当前页面URL并判断URL中是否含有linkurl
+        if (window.location.href.indexOf(linkurl) != -1) {
+            // 如果包含则,设置class
+            links[i].className = "here";
+        }
+    }
+}
+
+// 添加选中状态函数
+addLoadEvent(hightlightPage);
