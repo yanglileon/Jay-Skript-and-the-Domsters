cf388bcbdd9e4743a7421d561af0fd81e1829209
李洋 <yang.li.lc.leon@gmail.com>
2017-08-05 13:59:53 +0800

    增强表格完成

--- Jay Skript and the Domsters/scripts/global.js
+++ Jay Skript and the Domsters/scripts/global.js
@@ -229,6 +229,7 @@ function preparePlaceholder() {
  */
 function prepareGallery() {
     if (!document.getElementsByTagName) return false;
+    if (!document.getElementById("imagegaller")) return false;
     var imagegallery = document.getElementById("imagegallery");
     var links = imagegallery.getElementsByTagName("a");
     if (links.length == 0) return false;
@@ -263,9 +264,88 @@ function showPic(whichPic) {
     return true;
 }
 
+/**
+ * 设置表格行的class
+ */
+function stripeTables() {
+    if (!document.getElementsByTagName) return false;
+    var tables = document.getElementsByTagName("table");
+    for (var i = 0; i < tables.length; i++) {
+        var odd = false;
+        var rows = tables[i].getElementsByTagName("tr");
+        for (var j = 0; j < rows.length; j++) {
+            if (odd == true) {
+                addClass(rows[j], "odd");
+                odd = false;
+            } else {
+                odd = true;
+            }
+        }
+    }
+}
+
+/**
+ * 添加表格状态
+ */
+function highlightRows() {
+    if (!document.getElementsByTagName) return false;
+    var rows = document.getElementsByTagName("tr");
+    for (var i = 0; i < rows.length; i++) {
+        rows[i].oldClassName = rows[i].className;
+        rows[i].onmouseover = function() {
+            addClass(this, "highlight");
+        }
+        rows[i].onmouseout = function() {
+            this.className = this.oldClassName;
+        }
+    }
+}
+
+/**
+ * 列表清单函数
+ */
+function displayAbbreviations() {
+    console.log("1");
+    if (!document.createElement || !document.createTextNode || !document.getElementsByTagName) return false;
+    var abbreviations = document.getElementsByTagName("abbr");
+    if (abbreviations.length < 1) return false;
+    var defs = new Array();
+    for (var i = 0; i < abbreviations.length; i++) {
+        var current_abbr = abbreviations[i];
+        if (current_abbr.childNodes.length < 1) return false;
+        var definition = current_abbr.getAttribute("title");
+        var key = current_abbr.lastChild.nodeValue;
+        defs[key] = definition;
+    }
+    var dist = document.createElement("dl");
+    for (key in defs) {
+        var definition = defs[key];
+        var dtitle = document.createElement("dt");
+        var dtitle_text = document.createTextNode(key);
+        dtitle.appendChild(dtitle_text);
+        var ddesc = document.createElement("dd");
+        var ddesc_text = document.createTextNode(definition);
+        ddesc.appendChild(ddesc_text);
+        dist.appendChild(dtitle);
+        dist.appendChild(ddesc);
+    }
+    if (dist.childNodes.length < 1) return false;
+    var header = document.createElement("h3");
+    var header_text = document.createTextNode("Abbreviations");
+    header.appendChild(header_text);
+    var articles = document.getElementsByTagName("article");
+    if (articles.length == 0) return false;
+    var container = articles[0];
+    container.appendChild(header);
+    container.appendChild(dist);
+}
+
 // 添加选中状态函数
 addLoadEvent(highlightPage);
 addLoadEvent(prepareSlideshow);
 addLoadEvent(prepareInternalnav);
 addLoadEvent(preparePlaceholder);
 addLoadEvent(prepareGallery);
+addLoadEvent(stripeTables);
+addLoadEvent(highlightRows);
+addLoadEvent(displayAbbreviations);
